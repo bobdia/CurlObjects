c1107237f9cfc5caf49df25141ba406e45aa0498
Bob Diaes <github@disattention.com>
2011-01-20 19:08:00 +0200

    removing files"

--- util/parseForms.php
+++
@@ -1,8 +0,0 @@
-public static function parseForms($req){
-	//$req can be html or HttpRequest Object
-	if($req instanceof HttpRequest) $body = (string) $req->body;
-	else $body = $req;
-
-	$parser = new HtmlFormParser( $body );
-	return $parser->parseForms();
-}
